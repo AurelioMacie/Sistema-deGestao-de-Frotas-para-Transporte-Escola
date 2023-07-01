205cecd24ff88765222e6c1461c92be3f5301eed
AurelioMAcie <FAMACIE24@GMAIL.COM>
2023-07-01 20:20:04 +0200

    adicionadas rotas no ficheiro web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\EscolaController;
+use App\Http\Controllers\OficinaController;
+use App\Http\Controllers\MotoristaController;
+use App\Http\Controllers\VeiculoController;
+use App\Http\Controllers\ManutencaoController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +35,47 @@ Route::prefix('dashboard')->group(function () {
 Route::view('default-layout', 'multiple.default-layout')->name('default-layout');
 Route::view('compact-layout', 'multiple.compact-layout')->name('compact-layout');
 Route::view('modern-layout', 'multiple.modern-layout')->name('modern-layout');
+
+
+Route::prefix('escola')->group(function(){
+	Route::get('/', [EscolaController::class, 'index'])->name('escola');
+    Route::get('/create', [EscolaController::class, 'create'])->name('escola-create');
+    Route::post('/', [EscolaController::class, 'store'])->name('escola-store');
+    Route::get('/{id}/edit', [EscolaController::class, 'edit'])->name('escola-edit');
+    Route::put('/{id}', [EscolaController::class, 'update'])->name('escola-update');
+    Route::delete('/{id}', [EscolaController::class, 'destroy'])->name('escola-destroy');
+});
+
+Route::prefix('oficina')->group(function(){
+    Route::get('/', [OficinaController::class, 'index'])->name('oficina');
+    Route::get('/create', [OficinaController::class, 'create'])->name('oficina-create');
+    Route::post('/', [OficinaController::class, 'store'])->name('oficina-store');
+    Route::get('/{id}/edit', [OficinaController::class, 'edit'])->name('oficina-edit');
+    Route::put('/{id}', [OficinaController::class, 'update'])->name('oficina-update');
+    Route::delete('/{id}', [OficinaController::class, 'destroy'])->name('oficina-destroy');
+});
+
+Route::prefix('motorista')->group(function(){
+    Route::get('/', [MotoristaController::class, 'index'])->name('motorista');
+    Route::get('/create', [MotoristaController::class, 'create'])->name('motorista-create');
+    Route::post('/', [MotoristaController::class, 'store'])->name('motorista-store');
+    Route::get('/{id}/edit', [MotoristaController::class, 'edit'])->name('motorista-edit');
+    Route::put('/{id}', [MotoristaController::class, 'update'])->name('motorista-update');
+    Route::delete('/{id}', [MotoristaController::class, 'destroy'])->name('motorista-destroy');
+});
+
+
+Route::prefix('veiculo')->group(function(){
+    Route::get('/', [VeiculoController::class, 'index'])->name('veiculo');
+    Route::get('/create', [VeiculoController::class, 'create'])->name('veiculo-create');
+    Route::post('/', [VeiculoController::class, 'store'])->name('veiculo-store');
+    Route::get('/{id}/edit', [VeiculoController::class, 'edit'])->name('veiculo-edit');
+    Route::put('/{id}', [VeiculoController::class, 'update'])->name('veiculo-update');
+    Route::delete('/{id}', [VeiculoController::class, 'destroy'])->name('veiculo-destroy');
+});
+
+Route::prefix('manutencao')->group(function(){
+    Route::get('/', [ManutencaoController::class, 'index'])->name('manutencao');
+    Route::get('/create', [ManutencaoController::class, 'create'])->name('manutencao-create');
+    Route::post('/', [ManutencaoController::class, 'store'])->name('manutencao-store');
+});
